0e4adc6feb757cd4555ae156dd13a69a2159102d
Daria Vinogradova <vinodarya@yandex.ru>
2022-01-16 23:50:14 +0300

    feat: добавить функции работы с формой

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,40 @@
 let content = document.querySelector('.content');
 let popup = content.querySelector('.popup');
 let popupForm = content.querySelector('.popup__form');
-let popupName = content.querySelector('.popup__name');
-let popupDesc = content.querySelector('.popup__description');
 
 let editButton = content.querySelector('.profile__edit-button');
 let closeButton = content.querySelector('.popup__close');
 
+//Отобразить форму с заполненными полями
 function popupOpen() {
-  let name = content.querySelector('.profile__name');
-  let description = content.querySelector('.profile__description');
+  let name = document.querySelector('.profile__name');
+  let description = document.querySelector('.profile__description');
+  let popupName = document.querySelector('.popup__name');
+  let popupDesc = document.querySelector('.popup__description');
+
+  popupName.value = name.textContent;
+  popupDesc.value = description.textContent;
 
   popup.classList.add('popup_opened');
-
-  popupName.setAttribute('value', name.textContent);
-  popupDesc.setAttribute('value', description.textContent);
 }
 
+//Закрыть форму
 function popupClose() {
   popup.classList.remove('popup_opened');
 }
 
+//Закрыть форму с перезаписью значений на главной
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+  evt.preventDefault();
 
-    // Получите значение полей jobInput и nameInput из свойства value
+  let popupName = document.querySelector('.popup__name');
+  let popupDesc = document.querySelector('.popup__description');
+  let name = document.querySelector('.profile__name');
+  let description = document.querySelector('.profile__description');
 
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
-    popupClose();
+  name.textContent = popupName.value;
+  description.textContent = popupDesc.value;
+  popupClose();
 }
 
 editButton.addEventListener('click', popupOpen);
