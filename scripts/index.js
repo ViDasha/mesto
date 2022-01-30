cfbfe5061e450d6a7df8e2f2ef5440205d3fd963
Daria Vinogradova <vinodarya@yandex.ru>
2022-01-31 00:19:27 +0300

    feat: добавление карточек при загрузке страницы

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 let page = document.querySelector('.page');
 let popup = page.querySelector('.popup');
 let popupForm = page.querySelector('.popup__form');
@@ -10,6 +37,21 @@ let profileJob = page.querySelector('.profile__job');
 let popupName = document.getElementById('pp-name');
 let popupJob = document.getElementById('pp-job');
 
+//Загрузить 6 карточек формы
+function loadInitialCards() {
+  const elementTemplate = document.querySelector('#element').content;
+  const elements = document.querySelector('.elements');
+  
+  initialCards.forEach(item => {
+    const element = elementTemplate.querySelector('.element').cloneNode(true);
+    element.querySelector('.element__image').src = item.link;
+    element.querySelector('.element__image').alt = item.name;
+    element.querySelector('.element__name').textContent = item.name;
+
+    elements.append(element);
+  });
+
+}
 //Отобразить форму с заполненными полями
 function openPopup() {
   popupName.value = profileName.textContent;
@@ -32,6 +74,7 @@ function formSubmitHandler (evt) {
   closePopup();
 }
 
+loadInitialCards();
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', formSubmitHandler);
