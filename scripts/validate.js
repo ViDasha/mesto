a289715eee5f4e3f9f3d7fed0a666ae8d3a2b5fe
Daria Vinogradova <vinodarya@yandex.ru>
2022-02-17 00:14:21 +0300

    feat: добавить валидацию всем полям edit попапа

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,30 +1,29 @@
-const formInput = popupEditForm.querySelector('.popup__item');
-const formError = popupEditForm.querySelector(`.${formInput.id}-error`);
-    
   // Функция, которая добавляет класс с ошибкой
-const showInputError = (element, errorMessage) => {
-  element.classList.add('popup__item_type_error');
-      // Заменим содержимое span с ошибкой на переданный параметр
-  formError.textContent = errorMessage;
-  formError.classList.add('popup__item-error_active');
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__item_type_error');
+  // Заменим содержимое span с ошибкой на переданный параметр
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__item-error_active');
 };
   
   // Функция, которая удаляет класс с ошибкой
-const hideInputError = (element) => {
-  element.classList.remove('popup__item_type_error');
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__item_type_error');
   //Удаляем span с ошибкой
-  formError.classList.remove('popup__item-error_active');
-  formError.textContent = "";
+  errorElement.classList.remove('popup__item-error_active');
+  errorElement.textContent = "";
 };
   
   // Функция, которая проверяет валидность поля
-const isValid = () => {
+const isValid = (formElement, formInput) => {
   if (!formInput.validity.valid) {
     // Если поле не проходит валидацию, покажем ошибку
-    showInputError(formInput, formInput.validationMessage);
+    showInputError(formElement, formInput, formInput.validationMessage);
   } else {
     // Если проходит, скроем
-    hideInputError(formInput);
+    hideInputError(formElement, formInput);
   }
 };
 
@@ -32,9 +31,24 @@ popupEditForm.addEventListener('submit', function (evt) {
     // Отменим стандартное поведение
   evt.preventDefault();
 });
-    
-  // Слушатель события input
-formInput.addEventListener('input', isValid);
+
+const setEventListeners = (formElement) => {
+    // Находим все поля внутри формы,
+    // сделаем из них массив методом Array.from
+    const inputList = Array.from(formElement.querySelectorAll('.popup__item'));
+  
+    // Обойдём все элементы полученной коллекции
+    inputList.forEach((inputElement) => {
+      // каждому полю добавим обработчик события input
+      inputElement.addEventListener('input', () => {
+        // Внутри колбэка вызовем isValid,
+        // передав ей форму и проверяемый элемент
+        isValid(formElement, inputElement)
+      });
+    });
+  }; 
+
+  setEventListeners(popupEditForm);
 
  /*function enableValidation(popup, {
     formSelector: '.popup__form',
