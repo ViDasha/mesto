f6474d012670a8fe48bc39f6c2b86e3a264025b7
Daria Vinogradova <vinodarya@yandex.ru>
2022-02-19 00:24:47 +0300

    fix: rename popup opened

--- scripts/index.js
+++ scripts/index.js
@@ -155,8 +155,8 @@ popupEditForm.addEventListener('submit', rewriteProfile);
 
 document.addEventListener('keydown', function (evt) {
   if ((evt.key === "Escape") || (evt.key === "Esc")) {
-    const popupr = document.getElementsByClassName('popup_opened');
-    switch (popupr[0].id) { 
+    const popupOpened = document.getElementsByClassName('popup_opened');
+    switch (popupOpened[0].id) { 
       case 'pp-add': closePopupAdd();
       case 'pp-edit': closePopupEdit();
       case 'pp-img': closePopupImg();
