f707940c18184d993586e8b97c8911c561ddd9af
Daria Vinogradova <vinodarya@yandex.ru>
2022-03-30 14:45:23 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
         exclude: '/node_modules/'
       },
       {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource',
       },
