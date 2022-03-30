20b7bcf91ae125890168c3ab6f7c93b907a72289
Daria Vinogradova <vinodarya@yandex.ru>
2022-03-30 13:47:06 +0300

    fix: убрать html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -26,10 +26,6 @@ module.exports = {
         exclude: '/node_modules/'
       },
       {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource',
       },
